94ea98a7de0f9f73afd98fece63390d9797af6b7
emit1892 <emitem1892@gmail.com>
2023-03-09 22:30:54 +0900

    コメントアウトしたソースを削除

--- SlackAndBacklog/__init__.py
+++ SlackAndBacklog/__init__.py
@@ -8,27 +8,6 @@ BASE_URL = 'https://{backlog_space_id}.backlog.com/api/v2/{api}'
 api_key = os.environ.get("BACKLOG_TOKEN")
 backlog_space_key = os.environ.get("BACKLOG_SPACE_KEY")
 
-# def add_comment(_comment, _issue_id_key):
-#     api = f'issues/{_issue_id_key}/comments'
-#     url = BASE_URL.format(backlog_space_id=backlog_space_key, api=api)
-    
-#     params = {
-#         'apiKey': api_key
-#     }
-    
-#     headers = {
-#         'Content-Type': 'application/x-www-form-urlencoded'
-#     }
-    
-#     payload = {
-#         'content': _comment
-#     }
-    
-#     response = requests.post(url, params=params, headers=headers, data=payload)
-#     response.raise_for_status()
-    
-#     return response
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
