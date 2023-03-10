77d9d02a2698dc5e3ed73eb519b052088c3e426f
emit1892 <emitem1892@gmail.com>
2023-03-10 17:45:19 +0900

    Slackからslash commandを投稿した情報をAzure Functionsで受け取り解析する

--- SlackAndBacklog/__init__.py
+++ SlackAndBacklog/__init__.py
@@ -10,27 +10,32 @@ backlog_space_key = os.environ.get("BACKLOG_SPACE_KEY")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    
+    logging.info(req)
     issue_id_key = 'EMPROJECT-1'
     comment = '''
         test
         テスト
         # test
     '''
-    comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    #comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)    
+    url = req.url
+    logging.info(url)
+    
+    str_req_body = req.get_body().decode('utf-8')
+    list_req_body = str_req_body.split("&")
+    
+    dic_req_body = {}
+    for req_body in list_req_body:
+        key_value_req_body = req_body.split("=")
+        dic_req_body[key_value_req_body[0]] = key_value_req_body[1]
+    
+    logging.info(dic_req_body)
+    
+    channel_id = dic_req_body['channel_id']
+    if not channel_id:
+        return func.HttpResponse(f"channel_idが取得できませんでした。")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"{channel_id}, This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
