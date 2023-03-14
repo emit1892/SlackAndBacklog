6ad5d2f2c80346cd4b8e69949e02ec43a6dfc032
emit1892 <emitem1892@gmail.com>
2023-03-14 23:16:42 +0900

    課題キーが存在しない場合のエラー処理実装

--- SlackAndBacklog/__init__.py
+++ SlackAndBacklog/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 import requests
 from SlackAndBacklog import comments
+from SlackAndBacklog import issues
 import azure.functions as func
 
 BASE_URL = 'https://{backlog_space_id}.backlog.com/api/v2/{api}'
@@ -24,6 +25,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # slackに投稿
     logging.info(f'課題キー: {issue_id_key}')
     logging.info(f'コメント内容: {comment}')
-    comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)
     
-    return func.HttpResponse('' ,status_code=200)
+    issues_info = issues.get_issues_info(issue_id_key)
+    
+    if 'errors' in issues_info:
+        return func.HttpResponse(f'{issue_id_key} は存在しません。' ,status_code=200)
+    else:
+        #comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)
+        logging.info(issues_info['issueKey'])
+        return func.HttpResponse('' ,status_code=200)
--- SlackAndBacklog/issues.py
+++ SlackAndBacklog/issues.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import logging
 
 BASE_URL = 'https://{backlog_space_id}.backlog.com/api/v2/{api}'
 
@@ -32,14 +33,18 @@ def add_issue(_project_key, _issue_type_id, _priority_id, _summary, _description
     
     return response
 
-project_id = 375344
-issue_type_id = 1891580
-priority_id = 3
-summary = 'test'
-description = '''
-    test
-    テスト
-    # test
-'''
-
-response = add_issue(project_id, issue_type_id, priority_id, summary, description)
+def get_issues_info(_issue_id_key):
+    issue_id_key = _issue_id_key
+    api = 'issues'
+    url = BASE_URL.format(backlog_space_id=backlog_space_key, api=api)
+    
+    params = {
+        'apiKey': api_key,
+        'issueIdOrKey': issue_id_key
+    }
+    
+    response = requests.post(url, params=params)
+    issues_info = response.json()
+    logging.info(issues_info)
+    
+    return issues_info
