210e95f62c19b395e3a5b1048458418b8dabc49f
emit1892 <emitem1892@gmail.com>
2023-03-15 22:44:05 +0900

    課題キーをSlackで指定しなかった場合のエラー処理実装

--- SlackAndBacklog/__init__.py
+++ SlackAndBacklog/__init__.py
@@ -14,18 +14,24 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # リクエストボディからメンション情報を取得
     slack_req_json = req.get_json()
     logging.info(slack_req_json)
-    
-    # 投稿するBacklogの課題キー
-    issue_id_key = slack_req_json['event']['text'].split(' ')[1]
-    logging.info(issue_id_key)
 
     # slack情報から投稿コメントを作成
     comment = comments.create_comment(slack_req_json)
+    
+    # 投稿するBacklogの課題キー
+    slack_text = slack_req_json['event']['text'].split(' ')
+    issue_id_key = ''
+    if len(slack_text) >= 1:
+        issue_id_key = slack_text[1]
+        logging.info(issue_id_key)
     
     # slackに投稿
     logging.info(f'課題キー: {issue_id_key}')
     logging.info(f'コメント内容: {comment}')
     
+    if not issue_id_key:
+        return func.HttpResponse(f'課題キーを指定していません。' ,status_code=200)
+        
     issues_info = issues.get_issues_info(issue_id_key)
     
     if 'errors' in issues_info:
