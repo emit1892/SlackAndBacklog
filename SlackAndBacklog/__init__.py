d5384f60ed466cacd1a6f0434f8d4b1aa721d821
emit1892 <emitem1892@gmail.com>
2023-03-15 22:56:17 +0900

    全体的にエラー処理を実装

--- SlackAndBacklog/__init__.py
+++ SlackAndBacklog/__init__.py
@@ -10,33 +10,37 @@ api_key = os.environ.get("BACKLOG_TOKEN")
 backlog_space_key = os.environ.get("BACKLOG_SPACE_KEY")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    
-    # リクエストボディからメンション情報を取得
-    slack_req_json = req.get_json()
-    logging.info(slack_req_json)
+    try:
+        # リクエストボディからメンション情報を取得
+        slack_req_json = req.get_json()
+        logging.info(slack_req_json)
 
-    # slack情報から投稿コメントを作成
-    comment = comments.create_comment(slack_req_json)
-    
-    # 投稿するBacklogの課題キー
-    slack_text = slack_req_json['event']['text'].split(' ')
-    issue_id_key = ''
-    if len(slack_text) >= 1:
-        issue_id_key = slack_text[1]
-        logging.info(issue_id_key)
-    
-    # slackに投稿
-    logging.info(f'課題キー: {issue_id_key}')
-    logging.info(f'コメント内容: {comment}')
-    
-    if not issue_id_key:
-        return func.HttpResponse(f'課題キーを指定していません。' ,status_code=200)
+        # slack情報から投稿コメントを作成
+        comment = comments.create_comment(slack_req_json)
         
-    issues_info = issues.get_issues_info(issue_id_key)
-    
-    if 'errors' in issues_info:
-        return func.HttpResponse(f'{issue_id_key} は存在しません。' ,status_code=200)
-    else:
-        #comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)
-        logging.info(issues_info['issueKey'])
-        return func.HttpResponse('' ,status_code=200)
+        # 投稿するBacklogの課題キー
+        slack_text = slack_req_json['event']['text'].split(' ')
+        issue_id_key = ''
+        if len(slack_text) >= 1:
+            issue_id_key = slack_text[1]
+            logging.info(issue_id_key)
+        
+        # slackに投稿
+        logging.info(f'課題キー: {issue_id_key}')
+        logging.info(f'コメント内容: {comment}')
+        
+        if not issue_id_key:
+            return func.HttpResponse(f'課題キーを指定していません。' ,status_code=200)
+            
+        issues_info = issues.get_issues_info(issue_id_key)
+        
+        if 'errors' in issues_info:
+            return func.HttpResponse(f'{issue_id_key} は存在しません。' ,status_code=200)
+        else:
+            #comments.add_comment(comment, issue_id_key, api_key, backlog_space_key)
+            logging.info(issues_info['issueKey'])
+            return func.HttpResponse('' ,status_code=200)
+        
+    except Exception as e:
+        logging.info(e.message)
+        return func.HttpResponse(e.message ,status_code=500)
